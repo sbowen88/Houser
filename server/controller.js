4974092d1a8e898908203dff805df25548440860
sbowen88 <sbowey@gmail.com>
2018-04-20 17:55:44 -0600

    third commit

--- server/controller.js
+++ server/controller.js
@@ -32,7 +32,7 @@ module.exports = {
       })
       .catch(err => {
         console.log(err);
-        res.status(500).send("Please Register Now");
+        res.status(500).send("Please Register");
       });
   },
   getProperties: (req, res) => {
